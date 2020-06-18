958bf75491f7268744920896bf45eb031a68555c
Brian Sai <brianwsai@gmail.com>
2020-06-17 17:39:01 -0700

    Implemented cookie session and jwt for user authentication

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import 'express-async-errors';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
 
 import { currentUserRouter } from './routes/currentUser';
 import { signInRouter } from './routes/signIn';
@@ -10,7 +11,16 @@ import { signUpRouter } from './routes/signUp';
 import { errorHandler } from './middlewares/errorHandler';
 
 const app = express();
+
+app.set('trust proxy', true);
 app.use(bodyParser.json());
+
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true
+  })
+);
 
 app.use(currentUserRouter);
 app.use(signInRouter);
@@ -20,6 +30,10 @@ app.use(signUpRouter);
 app.use(errorHandler);
 
 const dbConnect = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined');
+  }
+
   try {
     await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
       useNewUrlParser: true,
@@ -27,7 +41,7 @@ const dbConnect = async () => {
       useCreateIndex: true
     });
   } catch (err) {
-    console.log(err);
+    console.error(err);
   }
 
   app.listen(3000, () => {
