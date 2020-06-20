8d1bd3731446636d0ad4a398d33ecd9f88f88dbf
Brian Sai <brianwsai@gmail.com>
2020-06-20 16:08:18 -0700

    Implemented request validation middleware in sign up service

--- auth/src/routes/signUp.ts
+++ auth/src/routes/signUp.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
 import jwt from 'jsonwebtoken';
 import { User } from '../models/user';
+import { validateRequest } from '../middlewares/validateRequest';
 import { RequestValidationError } from '../errors/requestValidationError';
 import { BadRequestError } from '../errors/badRequestError';
 
@@ -18,13 +19,8 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage('Password must be between 4 and 20 characters.')
   ],
+  validateRequest,
   async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    }
-
     const { email, password } = req.body;
 
     const existingUser = await User.findOne({ email });
