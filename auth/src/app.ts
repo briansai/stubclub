37740f0c1ec45005f422ab63e7468bf01b8f42f3
Brian Sai <brianwsai@gmail.com>
2020-06-21 15:58:00 -0700

    Refactored express app code for cooperate with supertest

---
+++ auth/src/app.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import 'express-async-errors';
+import bodyParser from 'body-parser';
+import cookieSession from 'cookie-session';
+
+import { currentUserRouter } from './routes/currentUser';
+import { signInRouter } from './routes/signIn';
+import { signOutRouter } from './routes/signOut';
+import { signUpRouter } from './routes/signUp';
+import { errorHandler } from './middlewares/errorHandler';
+
+const app = express();
+
+app.set('trust proxy', true);
+app.use(bodyParser.json());
+
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true
+  })
+);
+
+app.use(currentUserRouter);
+app.use(signInRouter);
+app.use(signOutRouter);
+app.use(signUpRouter);
+
+app.use(errorHandler);
+
+export { app };
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,33 +1,5 @@
-import express from 'express';
-import 'express-async-errors';
-import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
-
-import { currentUserRouter } from './routes/currentUser';
-import { signInRouter } from './routes/signIn';
-import { signOutRouter } from './routes/signOut';
-import { signUpRouter } from './routes/signUp';
-import { errorHandler } from './middlewares/errorHandler';
-
-const app = express();
-
-app.set('trust proxy', true);
-app.use(bodyParser.json());
-
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true
-  })
-);
-
-app.use(currentUserRouter);
-app.use(signInRouter);
-app.use(signOutRouter);
-app.use(signUpRouter);
-
-app.use(errorHandler);
+import { app } from './app';
 
 const dbConnect = async () => {
   if (!process.env.JWT_KEY) {
