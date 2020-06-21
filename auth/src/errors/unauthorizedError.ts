f0698188283de9db8f1884cef02741660ffa6441
Brian Sai <brianwsai@gmail.com>
2020-06-21 13:49:02 -0700

    Created unauthorized error, curren user middleware, and auth requirement middleware

---
+++ auth/src/errors/unauthorizedError.ts
@@ -0,0 +1,14 @@
+import { CustomError } from './customError';
+
+export class UnauthorizedError extends CustomError {
+  statusCode = 401;
+
+  constructor() {
+    super('Not Authorized');
+
+    Object.setPrototypeOf(this, UnauthorizedError.prototype);
+  }
+  serializeErrors() {
+    return [{ message: 'Not Authorized' }];
+  }
+}
