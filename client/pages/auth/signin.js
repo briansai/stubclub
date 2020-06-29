83adaaebc174e50cbfefacbadec555c33e73cf3d
Brian Sai <brianwsai@gmail.com>
2020-06-28 21:07:44 -0700

    Included a client side sign in form

--- client/pages/auth/signin.js
+++ client/pages/auth/signin.js
@@ -10,7 +10,7 @@ const inputText = {
 export default () => {
   const [{ email, password }, setState] = useState(inputText);
   const { doRequest, errors } = useRequest({
-    url: '/api/users/signup',
+    url: '/api/users/signin',
     method: 'post',
     body: {
       email,
@@ -31,7 +31,7 @@ export default () => {
 
   return (
     <form onSubmit={onSubmit}>
-      <h1>Sign Up</h1>
+      <h1>Sign In</h1>
       <div className="form-group">
         <label>Email Address</label>
         <input
@@ -52,7 +52,7 @@ export default () => {
         />
       </div>
       {errors}
-      <button className="btn btn-primary">Sign Up</button>
+      <button className="btn btn-primary">Sign In</button>
     </form>
   );
 };
