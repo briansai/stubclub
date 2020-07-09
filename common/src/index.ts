e514b966ad6d966f0670d9baa4adb22518c537c6
Brian Sai <brianwsai@gmail.com>
2020-07-09 15:53:25 -0700

    Logged error in error handling middleware

---
+++ common/src/index.ts
@@ -0,0 +1,11 @@
+export * from './errors/badRequestError';
+export * from './errors/customError';
+export * from './errors/databaseConnectionError';
+export * from './errors/notFoundError';
+export * from './errors/requestValidationError';
+export * from './errors/unauthorizedError';
+
+export * from './middlewares/currentUser';
+export * from './middlewares/errorHandler';
+export * from './middlewares/requireAuth';
+export * from './middlewares/validateRequest';
