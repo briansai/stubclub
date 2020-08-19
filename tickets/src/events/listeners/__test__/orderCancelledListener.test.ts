fd4efded6e6b7bc99dcc90f2081553f3493e40dc
Brian Sai <brianwsai@gmail.com>
2020-08-19 13:58:47 -0700

    Wrote logic and test for cancelled order listener

---
+++ tickets/src/events/listeners/__test__/orderCancelledListener.test.ts
@@ -0,0 +1,46 @@
+import { Message } from 'node-nats-streaming';
+import {
+  OrderCancelledEvent,
+  generateMongoId,
+  OrderStatus
+} from '@stubclub/common';
+import { OrderCancelledListener } from '../orderCancelledListener';
+import { natsWrapper } from '../../../natsWrapper';
+import { Ticket } from '../../../models/ticket';
+
+const setup = async () => {
+  const listener = new OrderCancelledListener(natsWrapper.client);
+  const ticket = Ticket.build({
+    title: 'potatos',
+    price: 200,
+    userId: '123'
+  });
+
+  ticket.set({ orderId: generateMongoId() }), await ticket.save();
+
+  const data: OrderCancelledEvent['data'] = {
+    id: generateMongoId(),
+    version: 0,
+    ticket: {
+      id: ticket.id
+    }
+  };
+
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn()
+  };
+
+  return { listener, ticket, data, msg };
+};
+
+it('updates the ticket, publishes an event, and acks the message', async () => {
+  const { listener, ticket, data, msg } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  const updatedTicket = await Ticket.findById(ticket.id);
+  expect(updatedTicket!.orderId).not.toBeDefined();
+  expect(msg.ack()).not.toHaveBeenCalled();
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
