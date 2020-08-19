f2fc9f093bfcdddd4d0d75cc898717b4ab5a190a
Brian Sai <brianwsai@gmail.com>
2020-08-19 13:43:14 -0700

    Implemented another test to make sure ticketUpdatedPublisher event is called when order is created

--- tickets/src/events/listeners/orderCreatedListener.ts
+++ tickets/src/events/listeners/orderCreatedListener.ts
@@ -2,6 +2,7 @@ import { Message } from 'node-nats-streaming';
 import { Listener, OrderCreatedEvent, Subjects } from '@stubclub/common';
 import { queueGroupName } from './queueGroupName';
 import { Ticket } from '../../models/ticket';
+import { TicketUpdatedPublisher } from '../publishers/ticketUpdatedPublisher';
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
@@ -17,6 +18,16 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
     ticket.set({ orderId: data.id });
 
     await ticket.save();
+
+    const { id, price, title, userId, orderId, version } = ticket;
+    await new TicketUpdatedPublisher(this.client).publish({
+      id,
+      price,
+      title,
+      userId,
+      orderId,
+      version
+    });
 
     msg.ack();
   }
