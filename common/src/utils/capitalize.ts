ca1b6be6700fe71ad1b45b29fd852c30578a9d72
Brian Sai <brianwsai@gmail.com>
2020-09-02 13:23:10 -0700

    Created function to capitalize words

---
+++ common/src/utils/capitalize.ts
@@ -0,0 +1,8 @@
+const capitalize = (str: string) => {
+  const sentence = [];
+  const words = str.split(' ');
+  for (let word of words) {
+    sentence.push(word[0].toUpperCase() + word.slice(1));
+  }
+  return sentence.join(' ');
+};
