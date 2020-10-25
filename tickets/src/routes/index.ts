562f4a33d02bf27b177d75597fa4bdb14579387d
Brian Sai <brianwsai@gmail.com>
2020-10-24 18:37:12 -0700

    Queried tickets by current user signed in and queried all tickets when signed out

--- tickets/src/routes/index.ts
+++ tickets/src/routes/index.ts
@@ -1,11 +1,28 @@
 import express, { Request, Response } from 'express';
 import { Ticket } from '../models/ticket';
 
+interface UserPayload {
+  id: string;
+  email: string;
+}
+
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: UserPayload;
+    }
+  }
+}
+
 const router = express.Router();
 
 router.get('/api/tickets/', async (req: Request, res: Response) => {
-  const ticket = await Ticket.find({ orderId: undefined }).sort({ date: -1 });
-
+  const ticket = req.currentUser
+    ? await Ticket.find({
+        userId: { $ne: req.currentUser.id },
+        orderId: undefined
+      })
+    : await Ticket.find({ orderId: undefined }).sort({ date: -1 });
   res.send(ticket);
 });
 
